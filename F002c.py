ba6da3731787252edf304bcfb2cb29c2b6670b1f
Janko-Petra <72135459+Janko-Petra@users.noreply.github.com>
2020-12-16 12:04:47 +0100

    Add files via upload

---
+++ F002c.py
@@ -0,0 +1,8 @@
+#F0002c: Hány sornyi kiírás, azaz szöveg és üres sor kerül a képernyőre, ha majd egyszer lefuttatod ezt a programot?
+
+
+print('/nElső sor\n')
+print('\nNos?/n')
+print('/nAzaz?\n')
+print('/nAkkor most mennyi?\n')
+print('Utolsó sor')
---
+++ F002d.py
@@ -0,0 +1,2 @@
+#F0002d: Rajzold ki egyetlen print() utasítással az alábbi ábrát! (Megoldás itt.) (Ennek a feladatnak – meg a következő kettőnek – az ötlete innen származik.)
+print('   /\n  /\n /\n')
